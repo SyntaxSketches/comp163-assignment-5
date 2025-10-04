4aad1df07a0a74e806e428f97cc861d5b1265895
Kabijah <kabijahhill2000@gmail.com>
2025-10-03 20:02:43 -0400

    Update knhill2_assignment_5.py

--- knhill2_assignment_5.py
+++ knhill2_assignment_5.py
@@ -1,1 +1,16 @@
+current_number = int(input("Enter starting number: "))
+step_count = 0
+
+print("Sequence:", end=" ")
+while current_number != 1:
+    print(current_number, end=" ")
+
+if current_number % 2 == 0:
+        current_number = current_number // 2
+else:
+        current_number = (current_number * 3) + 1
+        
+    step_count += 1
+print(current_number)
+print("Steps:", step_count)
 
